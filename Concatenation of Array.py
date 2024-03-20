1026961267121782bead82b5302cb738e0657185
Bereket Tadesse <106895952+berek47@users.noreply.github.com>
2024-03-20 18:39:02 +0300

    Create Concatenation of Array.py
    leetcode

---
+++ Concatenation of Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums * 2
